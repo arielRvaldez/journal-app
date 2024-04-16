8dca2751916195ed0ea2e3805160c711e23ea947
arielRvaldez <148600743+arielRvaldez@users.noreply.github.com>
2024-04-15 19:29:52 -0500

    vite install

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,20 +1,10 @@
-import ReactDOM from 'react-dom/client';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
 
-import App from './App.jsx';
-import Login from './Login.jsx';
-import Signup from './Signup.jsx';
-
-const router = createBrowserRouter([
-    {
-        path: '/',
-        component: <App />,
-        errorElement: <NoMatch />,  
-        children: [
-        { index: true, element: <Home />},
-        { path: 'login', element: Login },
-        { path: 'signup', component: Signup },
-        ],
-    },
-    ]);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
--- client/src/utils/actions.js
+++
@@ -1,4 +0,0 @@
-export const ADD_ENTRY = "ADD_ENTRY";
-export const ADD_GOAL = "ADD_GOAL";
-export const REMOVE_ENTRY = "REMOVE_ENTRY";
-export const UPDATE_ENTRY = "UPDATE_ENTRY";
