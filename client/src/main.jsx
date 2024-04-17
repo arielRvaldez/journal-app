6a64b616bacb337b0dd7942a664a874c9beaad53
JaronG548 <jarongarza548@gmail.com>
2024-04-16 21:51:34 -0500

    added Login-Signup and formatting

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
+import Home from './pages/Home.jsx'
 import './index.css'
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      }
+    ]
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={router} />
 )
