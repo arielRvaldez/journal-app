80921d0c03285d93a424682ca8ea35fae12ecc93
Domj1204 <148828606+Domj1204@users.noreply.github.com>
2024-04-22 15:19:08 -0500

    updated css values and app.css

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -15,7 +15,7 @@ function LandingPage() {
       </aside>
 
       <main className="main-content">
-        <h1>Welcome to My Journal App</h1>
+        <h1>Welcome to My ZenPages</h1>
         <p>If you already have an account, please <Link to="/login">login</Link>.</p>
         <p>If you are new here, you can <Link to="/signup">sign up</Link> for an account.</p>
         <Link to="/get-started" className="get-started-btn">Get Started</Link>
